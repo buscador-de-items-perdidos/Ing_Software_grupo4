44571eb65d22df223a18f4393899ea546e0efdeb
soda-caustica <maxbeltran@protonmail.com>
2025-10-28 23:29:55 -0300

    Se creó el botón flotante para crear reportes

--- lib/menu_reportes.dart
+++ lib/menu_reportes.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'modelos/reporte.dart';
 
-class MenuReportes extends StatefulWidget{
-  
+class MenuReportes extends StatefulWidget {
   const MenuReportes({super.key});
 
   @override
@@ -13,11 +12,29 @@ class MenuReportes extends StatefulWidget{
 class _MenuReportesState extends State<MenuReportes> {
   final List<Reporte> reportes = [];
 
-  @override Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Text"),),
-      body: ListView(
-      )
+      appBar: AppBar(title: Text("Menu de reportes"), centerTitle: true),
+      body: ListView(),
+      floatingActionButton: BotonCrear(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class BotonCrear extends StatelessWidget {
+  const BotonCrear({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      onPressed: () => debugPrint("abusadol"),
+      elevation: 3,
+      label: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Row(children: [Icon(Icons.add), const Text("Crear")]),
+      ),
     );
   }
 }
