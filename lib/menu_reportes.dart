290e1666b9ab67c57f5b7e0344933108db6c684d
soda-caustica <maxbeltran@protonmail.com>
2025-10-29 21:57:46 -0300

    Se generalizó el editor de reportes

--- lib/menu_reportes.dart
+++ lib/menu_reportes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ing_software_grupo4/editor_de_reportes.dart';
 import 'package:ing_software_grupo4/handlers/session_handler.dart';
 import 'package:ing_software_grupo4/menu_pendientes.dart';
+import 'package:ing_software_grupo4/modo.dart';
 
 import 'modelos/reporte.dart';
 import 'package:uuid/uuid.dart';
@@ -62,7 +63,8 @@ class BotonCrear extends StatelessWidget {
           MaterialPageRoute(
             builder: (_) {
               final String uuid = uuidGen.v7();
-              return EditorDeReportes.vacio(key: ValueKey(uuid), uuid);
+              return ReportDisplay.vacio(key: ValueKey(uuid), uuid, modo:
+                Modo.Editar);
             },
           ),
         );
