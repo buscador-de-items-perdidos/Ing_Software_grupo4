7ea03ca14783c948b1a354d089a7c33328f3a2df
soda-caustica <maxbeltran@protonmail.com>
2025-11-03 20:07:21 -0300

    Se agregaron sesiones a SessionHandler

--- lib/handlers/session_handler.dart
+++ lib/handlers/session_handler.dart
@@ -1,15 +1,25 @@
-
+import 'package:ing_software_grupo4/modelos/usuario.dart';
 
 abstract class SessionHandler {
-  //En teoria estos campos deberian empezar vacios y ser inicializados desde 
+  //En teoria estos campos deberian empezar vacios y ser inicializados desde
   //nuestra base de datos, pero para probar lo dejaremos así
-  static String nombreUsuario = 'Ying Wu Xie';
+  static final Map<String, Usuario> usuarios = {
+    "019a2e2f-d31c-7441-8355-62c252a55cc6": const Usuario(
+      nombreUsuario: "pandita_45",
+      correo: "jvidal@udec.cl",
+      numero: "+56 9 8417 9674",
+      miscelaneo: "Discord : pandita_45",
+    ),
+  };
+  static Usuario? get usuarioActual => usuarios[uuid];
+  static String get nombreUsuario => usuarioActual?.nombreUsuario ?? "";
   //NUNCA guardes los nombres de usuario en cosas que requieran identificación, guarden UUID
   static String uuid = '019a2e2f-d31c-7441-8355-62c252a55cc6';
-  static bool isAdmin = true; //Aca vendría bien un enum, si es que pusieramos distintos niveles de privilegio
+  static bool isAdmin =
+      true; //Aca vendría bien un enum, si es que pusieramos distintos niveles de privilegio
 
-  void initialize() async { //Este metodo deberia pedir las sesiones que tiene el sistema a una base de datos, en teoria obvio
+  void initialize() async {
+    //Este metodo deberia pedir las sesiones que tiene el sistema a una base de datos, en teoria obvio
     throw UnimplementedError();
   }
-  
 }
--- lib/modelos/usuario.dart
+++ lib/modelos/usuario.dart
@@ -1,13 +1,11 @@
 class Usuario {
   final String nombreUsuario;
-  final String uuid;
   final String correo;
   final String numero;
   final String miscelaneo;
 
   const Usuario({
     required this.nombreUsuario,
-    required this.uuid,
     required this.correo,
     required this.numero,
     required this.miscelaneo,
