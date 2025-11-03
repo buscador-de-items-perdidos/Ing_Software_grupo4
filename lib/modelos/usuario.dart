d532e7d4fb19b2bed1f659a0426144ff0cfcdb82
soda-caustica <maxbeltran@protonmail.com>
2025-11-03 19:59:34 -0300

    Se creo la clase de usuario

---
+++ lib/modelos/usuario.dart
@@ -0,0 +1,15 @@
+class Usuario {
+  final String nombreUsuario;
+  final String uuid;
+  final String correo;
+  final String numero;
+  final String miscelaneo;
+
+  const Usuario({
+    required this.nombreUsuario,
+    required this.uuid,
+    required this.correo,
+    required this.numero,
+    required this.miscelaneo,
+  });
+}
